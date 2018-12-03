797d04d00bc7c0619c75b4c69498b6d0e51801c0
jingzhe shu <shujingzhe1996@gmail.com>
2018-12-03 21:23:42 +0800

    debag gripper

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,3 +8,4 @@
 
 # modified environment variables
 export PWD="/home/jingzhe/WorkSpace/ROS/mp500lwa4d_robot_env/build"
+export ROS_PACKAGE_PATH="/home/jingzhe/WorkSpace/ROS/mp500lwa4d_robot_env/src:/home/jingzhe/WorkSpace/ROS/PR2/src:/home/jingzhe/catkin_ws/src:/home/jingzhe/WorkSpace/ROS/AlphaDemo/src:/home/jingzhe/WorkSpace/ROS/Rostutorials_HuCX/src:/opt/ros/kinetic/share"
